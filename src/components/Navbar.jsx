c57742dc35f5b95a258ebe6ffb02a04c7c281818
Fermented-coder <kuivalappa@gmail.com>
2024-09-27 12:03:05 +0300

    navbar bigger text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
-    <div className="w-full h-[72px] mx-auto">
-      <p className=" w-full mx-auto">
-        <Link to="/home">home </Link>
-        <Link to="/stories">stories</Link>
-      </p>
+    <div className="max-w-[1440px]  h-[72px] mx-auto">
+      <div className="w-[1440px] mx-auto">
+        <p className=" w-full mx-auto text-black text-4xl ">
+          <Link to="/home">home </Link>
+
+          <Link to="/stories">stories</Link>
+        </p>
+      </div>
     </div>
   );
 };
