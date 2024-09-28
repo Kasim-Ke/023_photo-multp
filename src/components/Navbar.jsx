b5fdf186ef58615e47bf809741181e9425a9b174
Fermented-coder <kuivalappa@gmail.com>
2024-09-28 13:25:45 +0300

    nav bar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
-    <div className="max-w-[1440px]  h-[72px] mx-auto">
-      <div className="w-[1440px] mx-auto">
+    <div className="w-full  h-[72px] mx-auto">
+      <div className="mx-auto">
         <p className=" w-full mx-auto text-black text-4xl ">
           <Link to="/home">home </Link>
 
